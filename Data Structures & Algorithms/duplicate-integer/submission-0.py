13d1469f471360317545a0c3dc1788cc3ebc53c6
Dhruv Kothari <dhruv.kothari180@nmims.edu.in>
2026-07-25 17:20:41 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen=set(nums)
+        if len(nums)!=len(seen):
+            return True
+        else:
+            return False
+        
